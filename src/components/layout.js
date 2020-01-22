53b10fc3dfe076305bdc1e0d1176020c37ded1fb
Dennis Jen <dsjen@media.mit.edu>
2020-01-22 15:55:08 -0500

    Added reactstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
+import { Container } from "reactstrap";
 
 import Header from "./header";
 import "./layout.css";
@@ -26,20 +20,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`
-        }}
-      >
+      <Container>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          Center for Civic Media
         </footer>
-      </div>
+      </Container>
     </>
   );
 };
