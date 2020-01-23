63b7cb31f388fc9236cc8c0152fdf3edf131f38d
Dennis Jen <dsjen@media.mit.edu>
2020-01-23 16:55:46 -0500

    Updates

--- styles-96ea1c23eb1c27c83239.js
+++ styles-96ea1c23eb1c27c83239.js
@@ -1,2 +1,5 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,p,w){}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  { "8ypT": function(n, p, w) {} }
+]);
 //# sourceMappingURL=styles-96ea1c23eb1c27c83239.js.map
