b9ea6ac3569c68100c0aa2bd0f50649065a873cb
Dennis Jen <dsjen@media.mit.edu>
2020-02-24 14:28:13 -0500

    Update landing page styles to match design

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,26 +9,33 @@ import "./style.scss";
 const Jumbotron = props => {
   const { siteTitle } = props;
   return (
-    <div className="policy-toggle-background">
+    <div className="landing-jumbotron">
       <Container fluid>
-        <Row className="p-5 align-middle text-center pm-jumbotron d-flex">
-          <Col md={6} className="d-flex flex-column">
-            <div>
-              <h1>
-                <span className="half-highlight">{siteTitle}</span>
-              </h1>
-            </div>
-          </Col>
-          <Col md={6} className="d-flex flex-column">
-            <div>
-              <p className="mt-3 text-left description">
-                Exploring how data privacy policies translates into design and
-                practice for librarians, pediatricians, lawyers, designers,
-                engineers, philosophers, etc.
-              </p>
-            </div>
-          </Col>
-        </Row>
+        <div className="cover">
+          <Row className="align-middle d-flex title-row">
+            <Col md={6} className="d-flex flex-column justify-content-start">
+              <div>
+                <h1>
+                  <span className="half-highlight">{siteTitle}</span>
+                </h1>
+              </div>
+            </Col>
+          </Row>
+          <Row className="align-middle text-center d-flex">
+            <Col
+              md={6}
+              className="d-flex offset-md-6 flex-column justify-content-between"
+            >
+              <div>
+                <p className="mt-3 text-left description">
+                  Exploring how data privacy policies translates into design and
+                  practice for librarians, pediatricians, lawyers, designers,
+                  engineers, philosophers, etc.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
         <Row className="py-5">
           <PolicyToggleButtons
             policies={policies}
