7887943869513c1836c18979f29d8ec70d802ad8
Dennis Jen <dsjen@media.mit.edu>
2020-02-27 12:39:19 -0500

    Updates

--- component---src-pages-about-js-a04d2b32f1106a4ed231.js
+++ component---src-pages-about-js-a04d2b32f1106a4ed231.js
@@ -1,2 +1,93 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,a,t){"use strict";t.r(a);t("q4sD");var n=t("q1tI"),i=t.n(n),r=t("Bl7J"),s=t("HAIo"),o=t("2qpI"),l=t("eKp/");a.default=function(){return Object(l.b)(l.a)?i.a.createElement("div",{className:"privacy-page"},i.a.createElement(o.a,{showHome:!0}),i.a.createElement(r.a,{backgroundClass:"secondary-background"},i.a.createElement(s.a,{standAlone:!0}))):i.a.createElement(i.a.Fragment,null,i.a.createElement("h1",null,"TBD"))}},HAIo:function(e,a,t){"use strict";var n=t("q1tI"),i=t.n(n),r=t("ok1R"),s=t("rhny");t("4GFT");a.a=function(e){var a=e.standAlone,t=i.a.createElement("span",{className:"half-highlight"},"About");return i.a.createElement("div",{id:"about",className:"about"},i.a.createElement(r.a,null,i.a.createElement(s.a,{xs:12},a&&i.a.createElement("h1",null,t),!a&&i.a.createElement("h2",null,t))),i.a.createElement(r.a,null,i.a.createElement(s.a,{xs:12,sm:6},i.a.createElement("p",{className:"subheading-2"},"Let’s Talk Privacy explores how the implementation of privacy and data governance policies might impact a variety of fields."),i.a.createElement("p",null,"There have been dozens of draft bills on privacy and data governance policies. How do these policies translate in practice? How they might affect us? We interviewed 41 people, including lawyers, designers, engineers, civil rights advocates, policymakers, and stewards of sensitive data: social workers, pediatricians, librarians. These conversations revealed 1) the diverse definitions and contexts of privacy 2) a variety of responses to recent data governance and privacy draft bills, and 3) the biggest privacy-related challenges (and possible ways to solve them) in different fields."),i.a.createElement("p",null,"We believe in transparency and in improving how we translate complex topics to the public. This project aims to better inform and engage everyday people -- the non-privacy related lawyers, technologists, researchers about some of the latest developments in policy. The project also aims to bring together cross-disciplinary insights to inform policymakers about how their bills can impact end users and people who manage sensitive data for a variety of marginalized and vulnerable communities."),i.a.createElement("p",null,"This research reflects only a small portion of the diverse perspectives we can bring to data governance and policy making. We hope to spur more conversations, reveal new insights, and prompt positive action."))))}}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [4],
+  {
+    "3XHS": function(e, a, t) {
+      "use strict";
+      t.r(a);
+      t("q4sD");
+      var n = t("q1tI"),
+        i = t.n(n),
+        r = t("Bl7J"),
+        s = t("HAIo"),
+        o = t("2qpI"),
+        l = t("eKp/");
+      a.default = function() {
+        return Object(l.b)(l.a)
+          ? i.a.createElement(
+              "div",
+              { className: "privacy-page" },
+              i.a.createElement(o.a, { showHome: !0 }),
+              i.a.createElement(
+                r.a,
+                { backgroundClass: "secondary-background" },
+                i.a.createElement(s.a, { standAlone: !0 })
+              )
+            )
+          : i.a.createElement(
+              i.a.Fragment,
+              null,
+              i.a.createElement("h1", null, "TBD")
+            );
+      };
+    },
+    HAIo: function(e, a, t) {
+      "use strict";
+      var n = t("q1tI"),
+        i = t.n(n),
+        r = t("ok1R"),
+        s = t("rhny");
+      t("4GFT");
+      a.a = function(e) {
+        var a = e.standAlone,
+          t = i.a.createElement(
+            "span",
+            { className: "half-highlight" },
+            "About"
+          );
+        return i.a.createElement(
+          "div",
+          { id: "about", className: "about" },
+          i.a.createElement(
+            r.a,
+            null,
+            i.a.createElement(
+              s.a,
+              { xs: 12 },
+              a && i.a.createElement("h1", null, t),
+              !a && i.a.createElement("h2", null, t)
+            )
+          ),
+          i.a.createElement(
+            r.a,
+            null,
+            i.a.createElement(
+              s.a,
+              { xs: 12, sm: 6 },
+              i.a.createElement(
+                "p",
+                { className: "subheading-2" },
+                "Let’s Talk Privacy explores how the implementation of privacy and data governance policies might impact a variety of fields."
+              ),
+              i.a.createElement(
+                "p",
+                null,
+                "There have been dozens of draft bills on privacy and data governance policies. How do these policies translate in practice? How they might affect us? We interviewed 41 people, including lawyers, designers, engineers, civil rights advocates, policymakers, and stewards of sensitive data: social workers, pediatricians, librarians. These conversations revealed 1) the diverse definitions and contexts of privacy 2) a variety of responses to recent data governance and privacy draft bills, and 3) the biggest privacy-related challenges (and possible ways to solve them) in different fields."
+              ),
+              i.a.createElement(
+                "p",
+                null,
+                "We believe in transparency and in improving how we translate complex topics to the public. This project aims to better inform and engage everyday people -- the non-privacy related lawyers, technologists, researchers about some of the latest developments in policy. The project also aims to bring together cross-disciplinary insights to inform policymakers about how their bills can impact end users and people who manage sensitive data for a variety of marginalized and vulnerable communities."
+              ),
+              i.a.createElement(
+                "p",
+                null,
+                "This research reflects only a small portion of the diverse perspectives we can bring to data governance and policy making. We hope to spur more conversations, reveal new insights, and prompt positive action."
+              )
+            )
+          )
+        );
+      };
+    }
+  }
+]);
 //# sourceMappingURL=component---src-pages-about-js-a04d2b32f1106a4ed231.js.map
