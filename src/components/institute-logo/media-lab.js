9ced1b3726fc027ccdf2790714338921b2f3a0b4
Dennis Jen <dsjen@media.mit.edu>
2020-05-27 16:15:42 -0400

    Added media lab logo

---
+++ src/components/institute-logo/media-lab.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import FluidImage from "../fluid-image";
+
+const MediaLabLogo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(
+        relativePath: { eq: "MIT_ML_Logo_K_RGB.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <FluidImage data={data} />
+  );
+};
+
+export default MediaLabLogo;
